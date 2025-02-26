b6ad15def26bd159a248cc8c60ab078cbb770ca2
fohy <A.K.Kiprianov@urfu.me>
2025-02-26 14:16:11 +0500

    Правка ошибки в архитиктуре проекта

--- robots/src/model/RobotModel.java
+++ robots/src/model/RobotModel.java
@@ -1,28 +1,107 @@
-package controller;
+package model;
 
-import model.RobotModel;
-import view.GameVisualizer;
+public class RobotModel {
+    private volatile double robotPositionX = 100;
+    private volatile double robotPositionY = 100;
+    private volatile double robotDirection = 0;
 
-import javax.swing.*;
-import java.util.Timer;
-import java.util.TimerTask;
+    private volatile int targetPositionX = 150;
+    private volatile int targetPositionY = 100;
 
-public class GameController {
-    private final RobotModel model;
-    private final GameVisualizer view;
-    private final Timer timer;
+    private static final double maxVelocity = 0.1;
+    private static final double maxAngularVelocity = 0.001;
 
-    public GameController(RobotModel model, GameVisualizer view) {
-        this.model = model;
-        this.view = view;
-        this.timer = new Timer("events generator", true);
+    public double getRobotPositionX() {
+        return robotPositionX;
+    }
 
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                model.updateModel();
-                view.repaint();
-            }
-        }, 0, 10);
+    public double getRobotPositionY() {
+        return robotPositionY;
+    }
+
+    public double getRobotDirection() {
+        return robotDirection;
+    }
+
+    public int getTargetPositionX() {
+        return targetPositionX;
+    }
+
+    public int getTargetPositionY() {
+        return targetPositionY;
+    }
+
+    public void setTargetPosition(int x, int y) {
+        targetPositionX = x;
+        targetPositionY = y;
+    }
+
+    public void updateModel() {
+        double distance = distance(targetPositionX, targetPositionY, robotPositionX, robotPositionY);
+        if (distance < 0.5) {
+            return;
+        }
+        double velocity = maxVelocity;
+        double angleToTarget = angleTo(robotPositionX, robotPositionY, targetPositionX, targetPositionY);
+        double angularVelocity = 0;
+        if (angleToTarget > robotDirection) {
+            angularVelocity = maxAngularVelocity;
+        }
+        if (angleToTarget < robotDirection) {
+            angularVelocity = -maxAngularVelocity;
+        }
+
+        moveRobot(velocity, angularVelocity, 10);
+    }
+
+    private void moveRobot(double velocity, double angularVelocity, double duration) {
+        velocity = applyLimits(velocity, 0, maxVelocity);
+        angularVelocity = applyLimits(angularVelocity, -maxAngularVelocity, maxAngularVelocity);
+        double newX = robotPositionX + velocity / angularVelocity *
+                (Math.sin(robotDirection + angularVelocity * duration) -
+                        Math.sin(robotDirection));
+        if (!Double.isFinite(newX)) {
+            newX = robotPositionX + velocity * duration * Math.cos(robotDirection);
+        }
+        double newY = robotPositionY - velocity / angularVelocity *
+                (Math.cos(robotDirection + angularVelocity * duration) -
+                        Math.cos(robotDirection));
+        if (!Double.isFinite(newY)) {
+            newY = robotPositionY + velocity * duration * Math.sin(robotDirection);
+        }
+        robotPositionX = newX;
+        robotPositionY = newY;
+        double newDirection = asNormalizedRadians(robotDirection + angularVelocity * duration);
+        robotDirection = newDirection;
+    }
+
+    private static double distance(double x1, double y1, double x2, double y2) {
+        double diffX = x1 - x2;
+        double diffY = y1 - y2;
+        return Math.sqrt(diffX * diffX + diffY * diffY);
+    }
+
+    private static double angleTo(double fromX, double fromY, double toX, double toY) {
+        double diffX = toX - fromX;
+        double diffY = toY - fromY;
+        return asNormalizedRadians(Math.atan2(diffY, diffX));
+    }
+
+    private static double asNormalizedRadians(double angle) {
+        while (angle < 0) {
+            angle += 2 * Math.PI;
+        }
+        while (angle >= 2 * Math.PI) {
+            angle -= 2 * Math.PI;
+        }
+        return angle;
+    }
+
+    private static double applyLimits(double value, double min, double max) {
+        if (value < min)
+            return min;
+        if (value > max)
+            return max;
+        return value;
     }
 }
