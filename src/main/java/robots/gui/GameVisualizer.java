15ed99fcfc794cf06021e8835b403a99a99f60cf
karina <pakholkova-karina@mail.ru>
2025-04-14 19:49:43 +0500

    new test for task 1

--- src/main/java/robots/gui/GameVisualizer.java
+++ src/main/java/robots/gui/GameVisualizer.java
@@ -25,10 +25,10 @@ public class GameVisualizer extends JPanel
 
     public volatile double m_robotPositionX = 100;
     public volatile double m_robotPositionY = 100;
-    private volatile double m_robotDirection = 0;
+    volatile double m_robotDirection = 0;
 
-    private volatile int m_targetPositionX = 150;
-    private volatile int m_targetPositionY = 100;
+    volatile int m_targetPositionX = 150;
+    volatile int m_targetPositionY = 100;
 
     private static final double maxVelocity = 0.1;
     private static final double maxAngularVelocity = 0.001;
@@ -129,7 +129,7 @@ public class GameVisualizer extends JPanel
         return value;
     }
 
-    private void moveRobot(double velocity, double angularVelocity, double duration)
+    void moveRobot(double velocity, double angularVelocity, double duration)
     {
         m_robotDirection = asNormalizedRadians(m_robotDirection + angularVelocity * duration);
         velocity = applyLimits(velocity, 0, maxVelocity);
