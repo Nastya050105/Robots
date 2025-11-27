7bda11d7c01180e5271f01883fcf550aee703ed3
Анастасия Анастасия <anastasiaermakova050@mail.ru>
2025-11-27 21:07:59 +0500

    feature/tests Add 2 test classes. Fix troubles in LogWindowSource

---
+++ src/test/java/LogWindowSourceTest.java
@@ -0,0 +1,60 @@
+import log.LogChangeListener;
+import log.LogLevel;
+import log.LogWindowSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LogWindowSourceTest {
+    private LogWindowSource logWindowSource;
+
+    @BeforeEach
+    public void setUp() {
+        logWindowSource = new LogWindowSource(5);
+    }
+
+    @Test
+    void testRegisterListener() {
+        LogChangeListener listener = () -> {
+        };
+        logWindowSource.registerListener(listener);
+        assertEquals(1, logWindowSource.getCountListener());
+    }
+
+    @Test
+    void testUnregisterListener() {
+        LogChangeListener listener = () -> {
+        };
+        logWindowSource.registerListener(listener);
+        logWindowSource.unregisterListener(listener);
+
+        assertEquals(0, logWindowSource.getCountListener());
+    }
+
+    @Test
+    void testAppend() {
+        LogChangeListener listener = () -> {
+        };
+        logWindowSource.registerListener(listener);
+        logWindowSource.append(LogLevel.Info, "message");
+        logWindowSource.append(LogLevel.Info, "message");
+        logWindowSource.append(LogLevel.Info, "message");
+        assertEquals(3, logWindowSource.size());
+    }
+
+    @Test
+    void testOverflowQueue() {
+        LogChangeListener listener = () -> {
+        };
+        logWindowSource.registerListener(listener);
+        logWindowSource.append(LogLevel.Info, "message");
+        logWindowSource.append(LogLevel.Info, "message");
+        logWindowSource.append(LogLevel.Info, "message");
+        logWindowSource.append(LogLevel.Info, "message");
+        logWindowSource.append(LogLevel.Info, "message");
+        logWindowSource.append(LogLevel.Info, "message");
+        logWindowSource.append(LogLevel.Info, "message");
+        assertEquals(5, logWindowSource.size());
+    }
+}
---
+++ src/test/java/RobotModelMovementTest.java
@@ -0,0 +1,23 @@
+import gui.models.Robot;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RobotModelMovementTest {
+    @Test
+    public void testRobotMovesTowardTarget() {
+        Robot model = new Robot();
+
+        double x0 = model.getX();
+        double y0 = model.getY();
+
+        model.setTarget(200, 200);
+
+        for (int i = 0; i < 200; i++) {
+            model.update(10);
+        }
+
+        assertTrue(model.getX() > x0);
+        assertTrue(model.getY() > y0);
+    }
+}
